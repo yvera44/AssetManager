c041546bb50945d1dfd10dffd6b1a73e8acb8aa3
YairVera <yairvera44@gmail.com>
2025-10-29 12:27:54 -0500

    create field members,constructors, getters and setters

--- src/main/java/com/pluralsight/House.java
+++ src/main/java/com/pluralsight/House.java
@@ -1,4 +1,49 @@
 package com.pluralsight;
 
-public class House {
+public class House extends Asset {
+
+    private String address;
+    private int condition;
+    private int squareFoot;
+    private int lotsize;
+
+    public House(String description, String dateAcquired, double originalCost, String address, int condition, int squareFoot, int lotsize) {
+        super(description, dateAcquired, originalCost);
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotsize = lotsize;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getLotsize() {
+        return lotsize;
+    }
+
+    public void setLotsize(int lotsize) {
+        this.lotsize = lotsize;
+    }
 }
--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,39 @@
 package com.pluralsight;
 
-public class Vehicle {
+public class Vehicle extends Asset {
+
+    private String makeModel;
+    private int year;
+    private int odometer;
+
+    public Vehicle(String description, String dateAcquired, double originalCost, String makeModel, int year, int odometer) {
+        super(description, dateAcquired, originalCost);
+        this.makeModel = makeModel;
+        this.year = year;
+        this.odometer = odometer;
+    }
+
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
 }
